73ee9f77127c21b4bf1400cd02d9de95f1f257d6
Aditya Shahare <shahare.aditya@gmail.com>
2023-09-14 19:57:20 +0530

    chore: random movie number

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import VideoTitle from './VideoTitle';
 import VideoBackground from './VideoBackground';
 
 const MainContainer = () => {
+  const [movieId, setMovieId] = useState(null);
+
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 20);
+  };
+
+  useEffect(() => {
+    const id = getRandomNumber();
+    setMovieId(id);
+  }, []);
+
+  console.log(movieId)
 
   if (!movies) return;
 
-  const mainMovie = movies.at(6);
+  const mainMovie = movies.at(movieId);
 
   const { original_title, overview, id } = mainMovie;
 
